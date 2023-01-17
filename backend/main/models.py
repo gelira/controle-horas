c1b791636542388af913449f7a672c2368970d7a
Geraldo Eloi <geraldo.eloi11@gmail.com>
2023-01-16 23:04:53 -0300

    models

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from mongoengine import Document, fields
 
-# Create your models here.
+class WorkingDate(Document):
+    date = fields.StringField()
+    total_worked_minutes = fields.IntField(default=0)
+
+class WorkingTime(Document):
+    working_date = fields.ReferenceField(WorkingDate)
+    description = fields.StringField(default='')
+    start_time = fields.StringField(default='')
+    end_time = fields.StringField(default='')
+    worked_minutes = fields.IntField(default=0)
+    marked = fields.BooleanField(default=False)
