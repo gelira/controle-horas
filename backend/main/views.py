dea1f2d34397ee699ff9db6b4bfe6e952ecd6138
Geraldo Eloi <geraldo.eloi11@gmail.com>
2023-01-18 21:39:13 -0300

    create working time

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from ninja import NinjaAPI
 
-# Create your views here.
+from .models import WorkingDate, WorkingTime
+from .dto import CreateWorkingTimeDTO, WorkingTimeOutDTO
+
+api = NinjaAPI()
+
+@api.post('/working-time', response=WorkingTimeOutDTO)
+def create_working_time(request, payload: CreateWorkingTimeDTO):
+    wd = WorkingDate.get_or_create(payload.date)
+
+    wt = WorkingTime(working_date=wd)
+
+    for key, value in payload.dict().items():
+        if key != 'date':
+            setattr(wt, key, value) 
+
+    wt.save()
+
+    return wt
