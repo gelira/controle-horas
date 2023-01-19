27d19e07dfae648728b4c68592a40ccd979043af
Geraldo Eloi <geraldo.eloi11@gmail.com>
2023-01-18 22:27:32 -0300

    list working time

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,9 +1,25 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Query
 
 from .models import WorkingDate, WorkingTime
-from .dto import CreateWorkingTimeDTO, WorkingTimeOutDTO
+from .dto import (
+    WorkingDateQuery,
+    CreateWorkingTimeDTO, 
+    WorkingTimeOutDTO,
+    WorkingDateOutDTO
+)
 
 api = NinjaAPI()
+
+@api.get('/working-time', response=WorkingDateOutDTO)
+def list_working_time(request, query: WorkingDateQuery = Query(...)):
+    wd = WorkingDate.get_or_create(query.date)
+    qs = WorkingTime.objects(working_date=wd).order_by('start_time')
+
+    return {
+        'date': wd.date,
+        'total_worked_time': wd.total_worked_time,
+        'working_times': list(qs),
+    }
 
 @api.post('/working-time', response=WorkingTimeOutDTO)
 def create_working_time(request, payload: CreateWorkingTimeDTO):
